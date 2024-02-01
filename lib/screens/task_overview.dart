9a5947c270e6214cdc3b000d02d48aa20f1e13a1
Tommy131 <entwicklerKuhler3579@outlook.de>
2024-02-01 22:07:47 +0100

    feat: optimized UI & Color Design

--- lib/screens/task_overview.dart
+++ lib/screens/task_overview.dart
@@ -10,7 +10,7 @@
  * @Date         : 2024-01-19 00:55:40
  * @Author       : HanskiJay
  * @LastEditors  : HanskiJay
- * @LastEditTime : 2024-01-31 23:01:10
+ * @LastEditTime : 2024-02-01 22:02:18
  * @E-Mail       : support@owoblog.com
  * @Telegram     : https://t.me/HanskiJay
  * @GitHub       : https://github.com/Tommy131
@@ -21,6 +21,7 @@ import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:todolist_app/models/task.dart';
 import 'package:todolist_app/providers/todo_provider.dart';
+import 'package:todolist_app/widgets/capsule_tag.dart';
 
 class TaskOverViewScreen extends StatefulWidget {
   const TaskOverViewScreen({super.key});
@@ -45,18 +46,46 @@ class _TaskOverViewScreenState extends State<TaskOverViewScreen> {
             itemCount: sortedTasks.length,
             itemBuilder: (context, index) {
               Task task = sortedTasks[index];
-              Color? tileColor = _getTileColor(task);
+              Color tileColor = task.category.color;
               TextStyle textStyle = _getTextStyle(task);
 
               return Container(
-                color: tileColor,
+                color: _getTileColor(task, tileColor),
                 child: ListTile(
-                  title: Text(
-                    task.title,
-                    style: textStyle,
+                  title: Row(
+                    children: [
+                      task.isImportant
+                          ? CapsuleTag(
+                              text: 'Important',
+                              textColor:
+                                  task.isCompleted ? Colors.grey : Colors.red,
+                              backgroundColor: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            )
+                          : Container(),
+                      task.isImportant
+                          ? const SizedBox(width: 10.0)
+                          : Container(),
+                      CapsuleTag(
+                        text: task.category.name,
+                        textColor: task.isCompleted
+                            ? Colors.grey
+                            : task.category.color,
+                        backgroundColor: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      const SizedBox(width: 10.0),
+                      Text(
+                        task.title,
+                        style: textStyle,
+                      ),
+                    ],
                   ),
                   subtitle: Text(
                     'Due: ${DateFormat('yyyy-MM-dd').format(task.dueDate)}',
+                    style: TextStyle(
+                      color: task.isCompleted ? Colors.black45 : Colors.white,
+                    ),
                   ),
                   trailing: _buildTaskIconButton(task),
                 ),
@@ -83,33 +112,36 @@ class _TaskOverViewScreenState extends State<TaskOverViewScreen> {
       });
   }
 
-  Color? _getTileColor(Task task) {
-    if (task.isCompleted && !task.isImportant) {
-      return Colors.green.shade300;
-    } else if (!task.isCompleted && !task.isImportant) {
-      return Colors.blue.shade500;
-    } else if (!task.isCompleted && task.isImportant) {
-      return Colors.red.shade400;
-    } else if (task.isCompleted && task.isImportant) {
-      return Colors.pink.shade300;
+  Color _getTileColor(Task task, Color tileColor) {
+    if (task.isCompleted) {
+      return tileColor.withOpacity(0.5);
+    } else {
+      return tileColor.withOpacity(!task.isImportant ? 0.8 : 1);
     }
-    return null;
   }
 
   TextStyle _getTextStyle(Task task) {
     Color textColor = Colors.black;
     FontWeight fontWeight = FontWeight.normal;
+    TextDecoration decoration = TextDecoration.none;
 
-    if (!task.isCompleted && task.isImportant) {
-      fontWeight = FontWeight.bold;
+    if (task.isCompleted) {
+      textColor = textColor.withOpacity(0.6);
+      decoration = TextDecoration.lineThrough;
+      if (task.isImportant) {
+        fontWeight = FontWeight.bold;
+      }
+    } else {
       textColor = Colors.white;
-    } else if (task.isCompleted && task.isImportant) {
-      fontWeight = FontWeight.bold;
+      if (task.isImportant) {
+        fontWeight = FontWeight.bold;
+      }
     }
 
     return TextStyle(
       color: textColor,
       fontWeight: fontWeight,
+      decoration: decoration,
     );
   }
 
